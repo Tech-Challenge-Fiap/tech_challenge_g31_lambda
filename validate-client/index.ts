d0c370941c1ef05a6a2d47edc6c25e9b496faa88
unknown <erick_zen_@hotmail.com>
2024-03-17 19:21:29 -0300

    feat: adjust db connection

--- validate-client/index.ts
+++ validate-client/index.ts
@@ -3,25 +3,12 @@ import { APIGatewayProxyEvent,APIGatewayProxyResult } from 'aws-lambda';
 import { Client } from 'pg';
 import { generateJWT } from './utils/generateJwt';
 import { isValidCpf } from './utils/isValidCpf';
-import { IClient } from './interfaces/IClient';
-import { findClientByCpf } from './utils/findClientByCpf';
 import { randomBytes } from 'crypto';
 import dotenv from "dotenv";
 
 dotenv.config();
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // let response: APIGatewayProxyResult;
     const client = new Client({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
@@ -40,8 +27,16 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
 				body: JSON.stringify({ token: generateJWT(secretKey) }),
 			};
 		}
-        // WHEN CLIENT WANT TO GO AS ANONYMOUS
+        
+        await client.connect()
         const { cpf } = JSON.parse(event.body);
+        const query = 'SELECT * FROM clients WHERE cpf = $1';
+        const result = await client.query(query, [cpf]);
+
+        const clients = result.rows;
+        await client.end();
+
+        // WHEN CLIENT WANT TO GO AS ANONYMOUS
         if (cpf === undefined || cpf === "") {
 			return {
 				statusCode: 200,
@@ -50,35 +45,24 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
 		}
 
         if (!isValidCpf(cpf)) {
-			return {
+            return {
 				statusCode: 400,
 				body: JSON.stringify({ message: "CPF inválido" }),
 			};
-		} else {
-            await client.end();
-            await client.connect();
-    
-            const clienteTeste = await findClientByCpf(cpf, client) as IClient[];
-
-            await client.end();
-    
-            if (clienteTeste[0]) {
+        } else {
+            if (clients[0]) {
                 return {
                     statusCode: 200,
-                    body: JSON.stringify({
-                        token: generateJWT(secretKey, clienteTeste[0]),
-                    }),
-                };
+                    body: JSON.stringify({ token: generateJWT(secretKey, clients[0]) }),
+                }; 
             } else {
                 return {
                     statusCode: 404,
                     body: JSON.stringify({ message: "Cliente não encontrado" }),
-                };
+                }; 
             }
         }
-
     } catch (err: unknown) {
-        console.error(err);
         return {
             statusCode: 500,
             body: JSON.stringify({
